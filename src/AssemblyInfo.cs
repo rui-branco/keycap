3d79c0b6938fe51201e7cc8b7a5b1636493531f3
rui.branco <ruibranco.95@gmail.com>
2026-09-20 18:55:59 +0100

    chore: bump version to 1.1.6

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Keycap")]
 [assembly: AssemblyCompany("Rui Branco")]
 [assembly: AssemblyCopyright("(c) 2026")]
-[assembly: AssemblyVersion("1.1.5.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyVersion("1.1.6.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
 [assembly: ComVisible(false)]
